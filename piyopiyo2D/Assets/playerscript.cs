6aca2d4cdd5d44d5e261f43b3566a4cddafce445
marumarukan <kango.maruyama@gmail.com>
2017-12-06 20:18:08 +0900

    プレイヤーのジャンプ、横移動、壁生成

---
+++ piyopiyo2D/Assets/playerscript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerscript : MonoBehaviour {
+	public bool jump;
+	public float power = 2;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("space")) {
+			jump = true;
+		}
+		if (Input.GetKeyDown ("right")) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (3, 0);
+		}
+	}
+	void FixedUpdate(){
+		if (jump) {
+			jump = false;
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, 1) * power;
+		}
+	}
+}
---
+++ piyopiyo2D/Assets/wallsprict.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wallsprict : MonoBehaviour {
+
+
+	public GameObject wallPrefab;
+
+
+
+//	float nextSpwnTime=0;
+//	float interval = 2;
+//	public float speed = 2;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*if (nextSpwnTime < Time.timeSinceLevelLoad) {
+			nextSpwnTime = Time.timeSinceLevelLoad + interval;
+			LocalInstantate ();
+		}*/
+
+	}
+	void FixedUpdate(){
+		
+	}
+//	void LocalInstantate(){
+//		GameObject obj = (GameObject)GameObject.Instantiate (wallObject);
+//		obj.transform.parent = transform;
+//
+//		float y = Random.Range (3f, 10f);
+//		obj.transform.localPosition = new Vector2 (0, y);
+//
+//	}
+	void OnTriggerEnter2D(Collider2D collison2D){
+		if (collison2D.gameObject.tag == "Player") {
+			Instantiate (wallPrefab, this.gameObject.transform.position += new Vector3 (7, 0, 0), Quaternion.identity);
+			Destroy (this.gameObject);
+		}
+	}
+}
